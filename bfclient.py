a55ec632dca4c3a2d3b2d1ef8d6ef71c3f91009c
Danny Taing <dantaing@gmail.com>
2014-12-09 01:11:35 -0500

    added a Node class and improved DistanceVector class

--- bfclient.py
+++ bfclient.py
@@ -4,30 +4,53 @@ import socket
 import sys
 import datetime
 
-class DV:
-    """A class to represent a client neighbor in distance vector list"""
-    def __init__(self, ip_address, port, cost):
-        self.ip = ip_address
+class Node:
+    def __init__(self, ip, port, cost, link):
+        self.ip = ip
         self.port = port
         self.cost = cost
-        self.dest = ip_address + ":" + port
+        self.link = link
+
+    def __init__(self, ip, port, cost):
+        self.ip = ip
+        self.port = port
+        self.cost = cost
+        self.link = ""
+
+class DistanceVector:
+    def __init__(self, source, neighbors_dict):
+        self.source = source
+        # Initialize DV table with direct neighbors
+        self.dv_dict = neighbors_dict
+        for dest, neighbor in self.dv_dict:
+            neighbor.link = dest
+
+
+
+
+
 
 
 def main():
-    localport, timeout, neighbors_list = getArgs()
-    print localport
-    print timeout
-    print neighbors_list
-    dv_dict = {}
+    local_ip = "127.0.0.1"
+    local_port, timeout, neighbors_list = getArgs()
+
+    # Dictionary to maintain information only about directly attached neighbors
+    neighbors_dict = {}
     for i in range(0, len(neighbors_list), 3):
         ip = neighbors_list[i]
         port = neighbors_list[i+1]
         cost = neighbors_list[i+2]
-        temp_neighbor = DV(ip, port, cost)
-        dest = temp_neighbor.dest
-        dv_dict[dest] = temp_neighbor
+        dest = ip + ":" + port
+        temp_neighbor = Node(ip, port, cost)
+        neighbors_dict[dest] = temp_neighbor
 
-    printDVList(dv_dict)
+    # DV table initially contains only routing information about direct neighbors
+    localDV = DistanceVector(local_ip + ":" + local_port, neighbors_dict)
+    
+
+
+
 
 
 def getArgs():
@@ -38,11 +61,11 @@ def getArgs():
         neighbors_list.append(sys.argv[i])
     return localport, timeout, neighbors_list
 
-def printDVList(dv_dict):
+def printDVList(neighbors_dict):
     current_time = datetime.datetime.time(datetime.datetime.now().replace(microsecond=0))
     print "<" + str(current_time) + "> Distance vector list is:"
-    for dest, dv in dv_dict.iteritems():
-        print "Destination = " + dest + ", Cost = " + dv.cost + ", Link = "
+    for dest, dv in neighbors_dict.iteritems():
+        print "Destination = " + dest + ", Cost = " + dv + ", Link = "
 
 
 
