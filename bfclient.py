c3139d6c1b3efc7428d396c19d32ca7f651f24d9
Danny Taing <dantaing@gmail.com>
2014-12-09 02:36:45 -0500

    beginning to set up UDP sockets

--- bfclient.py
+++ bfclient.py
@@ -11,28 +11,52 @@ class Node:
         self.cost = cost
         self.link = link
 
-    def __init__(self, ip, port, cost):
+    def __init__(self, ip, port, cost, is_neighbor):
         self.ip = ip
         self.port = port
         self.cost = cost
         self.link = ""
+        self.is_neighbor = is_neighbor
 
 class DistanceVector:
     def __init__(self, source, neighbors_dict):
         self.source = source
         # Initialize DV table with direct neighbors
         self.dv_dict = neighbors_dict
-        for dest, neighbor in self.dv_dict:
+        for dest, neighbor in self.dv_dict.iteritems():
             neighbor.link = dest
+            neighbor.is_neighbor = True
+
+    def add_node(self, ip, port, cost, is_neighbor):
+        self.dv_dict[ip + ":" + port] = Node(ip, port, cost, is_neighbor)
+
+    def printDVList(self):
+        current_time = datetime.datetime.time(datetime.datetime.now().replace(microsecond=0))
+        print "<" + str(current_time) + "> Distance vector list is:"
+        for dest, dv in self.dv_dict.iteritems():
+            if dest != me:
+                print "Destination = " + dest + ", Cost = " + str(dv.cost) + ", Link = (" + dv.link + ")"
+
+def getArgs():
+    local_port = sys.argv[1]
+    timeout = sys.argv[2]
+    neighbors_list = []
+    for i in range(3, len(sys.argv)):
+        neighbors_list.append(sys.argv[i])
+    return local_port, timeout, neighbors_list
+
+def setup_server(host, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
+    print "listening on {0}:{1}\n".format(host, port)
+    return sock
+
+def broadcast_costs():
+    """ send estimated path costs to each neighbor """
 
 
-
-
-
-
-
-def main():
-    local_ip = "127.0.0.1"
+if __name__ == "__main__":
+    local_ip = socket.gethostbyname(socket.gethostname())
     local_port, timeout, neighbors_list = getArgs()
 
     # Dictionary to maintain information only about directly attached neighbors
@@ -42,35 +66,20 @@ def main():
         port = neighbors_list[i+1]
         cost = neighbors_list[i+2]
         dest = ip + ":" + port
-        temp_neighbor = Node(ip, port, cost)
+        temp_neighbor = Node(ip, port, cost, True)
         neighbors_dict[dest] = temp_neighbor
 
     # DV table initially contains only routing information about direct neighbors
-    localDV = DistanceVector(local_ip + ":" + local_port, neighbors_dict)
-    
+    myDV = DistanceVector(local_ip + ":" + local_port, neighbors_dict)
 
+    # Add myself to DV list
+    me = local_ip + ":" + local_port
+    myDV.add_node(local_ip, local_port, cost=0.0, is_neighbor=False)
+    myDV.printDVList()
 
+    # Begin accepting ROUTE UPDATES from neighbors
+    sock = setup_server(local_ip, int(local_port))
 
-
-
-def getArgs():
-    localport = int(sys.argv[1])
-    timeout = int(sys.argv[2])
-    neighbors_list = []
-    for i in range(3, len(sys.argv)):
-        neighbors_list.append(sys.argv[i])
-    return localport, timeout, neighbors_list
-
-def printDVList(neighbors_dict):
-    current_time = datetime.datetime.time(datetime.datetime.now().replace(microsecond=0))
-    print "<" + str(current_time) + "> Distance vector list is:"
-    for dest, dv in neighbors_dict.iteritems():
-        print "Destination = " + dest + ", Cost = " + dv + ", Link = "
-
-
-
-
-main()
 
 
 
